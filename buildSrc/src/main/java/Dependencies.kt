d053a7de4b9e669061217c0bb62fe975b2c85a77
Despair <p.a.makulin@gmail.com>
2022-10-09 18:38:59 +0300

    upd: изменил верию приложения в gradle.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -9,8 +9,8 @@ object Config {
 }
 
 object Releases {
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.01"
 }
 
 object Modules {
